3fe20d01d2a666ce6b26fd470e39d76fb8c789b8
kctang97 <42600194+kctang97@users.noreply.github.com>
2019-12-11 04:34:46 -0800

    new
    complete

--- student-work/game folder/game/sketch.js
+++ student-work/game folder/game/sketch.js
@@ -1,9 +1,9 @@
 'use strict';
 
-let char, charImg;
+let char, charImg, graduate;
 let state = 'titlePage';
 let cnv;
-let books0, books1, books3, dpls;
+let books1, books3, dpls, city;
 let cloud, bg;
 let book1 = [];
 let book2 = [];
@@ -16,16 +16,24 @@ let w = 1024;
 let h = 760;
 let diplomas = [];
 let points = 0;
-let jump, ps;
+let jump, ps, die, congrat;
+// let ms;
 
 function preload() {
+  //sound
   soundFormats('mp3', 'ogg');
+  // ms = loadSound("asset/sound/titlemus.mp3");
   jump = loadSound("asset/sound/jump.mp3");
   ps = loadSound("asset/sound/point.mp3");
+  die = loadSound("asset/sound/die.mp3");
+  congrat = loadSound('asset/sound/congrat.mp3');
+
+  //image
+  city = loadImage('asset/bg/city.png');
   charImg = createImg('asset/char/char.gif');
+  graduate = loadImage('asset/char/graduate.png');
   books1 = loadImage('asset/subject/book1.png');
   books3 = loadImage('asset/subject/book3.png');
-  books0 = loadImage('asset/subject/book0.png');
   cloud = loadImage('asset/subject/cloud1.png');
   dpls = loadImage('asset/subject/diploma.png');
   bg = loadImage('asset/bg/road.png');
@@ -38,12 +46,12 @@ function setup() {
 }
 
 function draw() {
-  // ms.play();
   switch (state) {
     case 'titlePage':
       titlePage();
       cnv.mouseClicked(function(){
         state = 'description';
+        // ms.stop();
       });
       break;
     case 'description':
@@ -60,11 +68,9 @@ function draw() {
       book2 = [];
       diplomas = [];
       points = 0;
-      if (key == 'r') {
-        state = 'titlePage';
-      }
       cnv.mouseClicked(function(){
-        state = 'titlePage';
+        die.stop();
+        state = 'game';
       });
 
       gameOver();
@@ -74,17 +80,24 @@ function draw() {
       book2 = [];
       diplomas = [];
       win();
+      cnv.mouseClicked(function(){
+        state = 'titlePage';
+      });
       break;
     default:
       break;
   }
 }
 
+// function loaded(){
+//   ms.play();
+// }
+
 function keyPressed() {
   switch (state) {
-
     case 'titlePage':
       if (keyCode === ENTER) {
+        // ms.stop();
         state = 'description';
       }
       break;
@@ -104,6 +117,21 @@ function keyPressed() {
       diplomas = [];
       points = 0;
       if (key == 'r') {
+        die.stop();
+        state = 'game';
+      }
+      if (keyCode == ESCAPE) {
+        die.stop();
+        state = 'titlePage';
+      }
+      break;
+    case 'win':
+      book1 = [];
+      book2 = [];
+      diplomas = [];
+      points = 0;
+      if (keyCode == ENTER) {
+        congrat.stop();
         state = 'titlePage';
       }
       break;
@@ -113,10 +141,9 @@ function keyPressed() {
 }
 
 function titlePage(){
-
-  // background(random(150, 200),random(150, 200),random(150, 200), 50);
   background(50, 150, 255);
   c();
+  road();
   fill(200, 0, 0);
   textStyle(BOLD);
   textSize(120);
@@ -124,10 +151,9 @@ function titlePage(){
   fill(random(50, 100), 0, 0, 30);
   textSize(125);
   text("Keep It Up", 200, 250);
-  fill(random(0, 200), 100);
+  fill(random(0, 200));
   textSize(40);
   text("~Enter or Click~ to Start", 270, 600);
-
 }
 
 function description() {
@@ -158,10 +184,10 @@ function description() {
   textSize(20);
   text("Statement: ", 50, 500);
   textSize(20);
-  text("The concept of this game is to avoid getting hit by books and bookshelves.", 50, 540);
-  text("The books and bookshelvess represented the workflow that we have to overcome in order to graduate.", 50, 570);
-  text("The idea of having the flashing background is an experiment of making eyes feel tired", 50, 600);
-  text("just like our eyes get tired after staring at the digital devices and books all day long.", 50, 630);
+  text("The play of this game is to avoid getting hit by books.", 50, 540);
+  text("The books represented the workflow that we have to overcome in order to graduate.", 50, 570);
+  text("The purpose of having the flashing background and motion blur is to make player's eyes get tired", 50, 600);
+  text("Just like our eyes get tired after staring at the digital devices and books for long period of time.", 50, 630);
   //epilepsy
   fill(random(0, 200), 150);
   textSize(32);
@@ -171,8 +197,8 @@ function description() {
 function game() {
   background (80, 130, random(100, 255), 70);
   fill(100, 50, 30);
-  road();
   c();
+  road();
 
   //blocks / books
   if (random(1) <= 0.009) {
@@ -185,6 +211,7 @@ function game() {
   }
   for (let i = book1.length - 1; i >= 0; i--){
     if(dist(char.x, char.y, book1[i].x, book1[i].y) <= (char.r + book1[i].r) / 3) {
+      die.play();
       state = 'game over';
     }
   }
@@ -199,6 +226,7 @@ function game() {
   }
   for (let i = book2.length - 1; i >= 0; i--){
     if(dist(char.x, char.y, book2[i].x, book2[i].y) <= (char.r + book2[i].r) / 3) {
+      die.play();
       state = 'game over';
     }
   }
@@ -208,13 +236,11 @@ function game() {
   //character
   char.show();
   char.move();
-  //distance
-
 }
 
 function road() {
-  image(bg, lf, 0, w, h);
-  image(bg, rt, 0, w, h);
+  image(bg, lf, 0, w+1, h);
+  image(bg, rt, 0, w+1, h);
 
   lf -= rSpeed;
   rt -= rSpeed;
@@ -231,6 +257,8 @@ function c() {
   //cloud
   image(cloud, lf, 0, w, h);
   image(cloud, rt, 0, w, h);
+  image(city, lf, 0, w, h - 70);
+  image(city, rt, 0, w, h - 70);
 
   lf -= cSpeed;
   rt -= cSpeed;
@@ -249,7 +277,7 @@ function dpa() {
   text(`Dipolmas: ${points}`, 20, 40);
 
   // //verticle diplomas
-  if (random(1) <= 0.003) {
+  if (random(1) <= 0.004) {
     diplomas.push(new Diploma());
   }
   for (let i = 0; i < diplomas.length; i++) {
@@ -266,30 +294,34 @@ function dpa() {
     }
   }
 
-  if (points >= 5){
+  if (points >= 4){
     state = 'win';
+    congrat.play();
   }
 }
 
 function gameOver() {
-  fill(200, 0, 0);
+  fill(200, 0, 0, 10);
   textStyle(BOLD);
-  textSize(120);
-  text("Dropped Out !", 130, 240);
-  fill(random(100, 200), 0, 0, 30);
-  textSize(125);
-  text("Dropped Out !", 140, 250);
-  fill(200);
+  textSize(100);
+  text("You Fail !", 290, 240);
+  fill(random(100, 190), 0, 0, 40);
+  textSize(130);
+  text("Classes Fail !", 100, 250);
+  fill(random(50, 200));
   textSize(30)
   text(" R to Restart", 400, 350);
+  text(" ESC to return Title Page", 320, 400);
+
 
   //hit the book for certain amount
 }
 
 function win() {
-  background(random(180, 255),random(180, 255), random(180, 255));
+  background(random(180, 255),random(180, 255), random(180, 255), 50);
   fill(random(100, 150),random(100, 150), random(100, 150));
   textStyle(BOLD);
   textSize(120);
-  text("Congratulation", 120, 240);
+  text("Congratulation !", 80, 240);
+  image(graduate, 300, 300);
 }
